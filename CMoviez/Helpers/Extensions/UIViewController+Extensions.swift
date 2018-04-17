9d6087c430464113416e128024f0d56206db8e56
Jobins John <jobins666@gmail.com>
2018-04-17 10:30:04 +0400

    New Group for Helpers in folder structure created

---
+++ CMoviez/Helpers/Extensions/UIViewController+Extensions.swift
@@ -0,0 +1,23 @@
+//
+//  UIViewController+Extensions.swift
+//  CMoviez
+//
+//  Created by Jobins John on 4/17/18.
+//  Copyright © 2018 Jobins John. All rights reserved.
+//
+
+import Foundation
+
+
+extension UIViewController
+{
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}

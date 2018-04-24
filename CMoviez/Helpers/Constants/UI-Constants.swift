b7f4a18d9a65487bd8e5c3900a8001a1a0ff824b
Jobins John <jobins666@gmail.com>
2018-04-24 09:39:43 +0400

    UI Constants updated

--- CMoviez/Helpers/Constants/UI-Constants.swift
+++ CMoviez/Helpers/Constants/UI-Constants.swift
@@ -12,17 +12,21 @@ import UIKit
 
 // MARK: - UI Color Constants
 
-let APP_MAIN_VIEW_BG_COLOR = ""
+let APP_MAIN_TB_VIEW_BG_COLOR       = "#333333"
 
 // MARK: - Font Color Constants
 
-let APP_MAIN_BTN_TITLE_FONT_COLOR = ""
+let APP_MAIN_BTN_TITLE_FONT_COLOR   = "#28bb4e"
 
-let APP_MAIN_FONT_COLOR = ""
+let APP_MOVIE_TITLE_FONT_COLOR      = "#ffffff"
+
+let APP_MOVIE_OVERVIEW_FONT_COLOR   = "#888888"
+
+let APP_MOVIE_RL_DATE_FONT_COLOR    = "#c7c7c7"
 
 // MARK: - Navigation Bar Color Constants
 
-let APP_MAIN_NAV_BAR_BG_COLOR = ""
+let APP_MAIN_NAV_BAR_BG_COLOR       = "#0d100e"
 
 // MARK: - Button Color Constants
 

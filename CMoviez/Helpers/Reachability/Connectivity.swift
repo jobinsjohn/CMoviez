f9cf8f18e840bf84ea7ae9c4c2f1c4a81e29173e
Jobins John <jobins666@gmail.com>
2018-04-24 11:30:34 +0400

    Connectivity class added

---
+++ CMoviez/Helpers/Reachability/Connectivity.swift
@@ -0,0 +1,17 @@
+//
+//  Connectivity.swift
+//  CMoviez
+//
+//  Created by Jobins John on 4/19/18.
+//  Copyright © 2018 Jobins John. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+
+class Connectivity {
+    class func isConnectedToInternet() ->Bool {
+        return NetworkReachabilityManager()!.isReachable
+    }
+}

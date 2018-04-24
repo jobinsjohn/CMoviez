d841aa92fca867f95bbac797fd52ec00c693d81a
Jobins John <jobins666@gmail.com>
2018-04-24 11:28:53 +0400

    Safe commit

---
+++ CMoviez/Helpers/Utilities/CFlixDefaultWrappers.swift
@@ -0,0 +1,20 @@
+//
+//  CFlixDefaultWrappers.swift
+//  CMoviez
+//
+//  Created by Jobins John on 4/24/18.
+//  Copyright © 2018 Jobins John. All rights reserved.
+//
+
+import UIKit
+
+class CFlixDefaultWrappers: NSObject {
+    
+    func showAlert(info:String, viewController: UIViewController)
+    {
+        let popUp = UIAlertController(title: APP_NAME, message: info, preferredStyle: UIAlertControllerStyle.alert)
+        popUp.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: {alertAction in popUp.dismiss(animated: true, completion: nil)}))
+        viewController.present(popUp, animated: true, completion: nil)
+    }
+
+}
---
+++ CMoviez/Search/CMSearchRecentHistoryTbCellController.swift
@@ -0,0 +1,26 @@
+//
+//  CMSearchRecentHistoryTbCellController.swift
+//  CMoviez
+//
+//  Created by Jobins John on 4/18/18.
+//  Copyright © 2018 Jobins John. All rights reserved.
+//
+
+import UIKit
+
+class CMSearchRecentHistoryTbCellController: UITableViewCell {
+
+    
+    @IBOutlet weak var rcntHisMovienameLabelOutlet: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        self.rcntHisMovienameLabelOutlet.textColor = UIColor(hexString: APP_MOVIE_TITLE_FONT_COLOR)
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        // Configure the view for the selected state
+    }
+}

51f6a1116083cb7bf41869c0256016590d26f336
Jobins John <jobins666@gmail.com>
2019-02-12 11:36:56 +0400

    Code Formatted

--- CMoviez/Helpers/Reachability/Connectivity.swift
+++ CMoviez/Helpers/Reachability/Connectivity.swift
@@ -9,9 +9,8 @@
 import Foundation
 import Alamofire
 
-
 class Connectivity {
-    class func isConnectedToInternet() ->Bool {
+    class func isConnectedToInternet() -> Bool {
         return NetworkReachabilityManager()!.isReachable
     }
 }

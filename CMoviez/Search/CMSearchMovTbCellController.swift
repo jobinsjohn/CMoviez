5995ca8b953b311733cc37a53dcbd5e3f265ab9e
Jobins John <jobins666@gmail.com>
2019-02-12 11:37:05 +0400

    Code Formatted

--- CMoviez/Search/CMSearchMovTbCellController.swift
+++ CMoviez/Search/CMSearchMovTbCellController.swift
@@ -9,30 +9,19 @@
 import UIKit
 
 class CMSearchMovTbCellController: UITableViewCell {
-
-    @IBOutlet weak var moviePosterImageView     : UIImageView!
-    
-    @IBOutlet weak var movieNameLabel           : UILabel!
-    
-    @IBOutlet weak var movieReleaseDateLabel    : UILabel!
-    
-    @IBOutlet weak var movieOverviewLabel       : UILabel!
-    
+    @IBOutlet weak var moviePosterImageView: UIImageView!
+    @IBOutlet weak var movieNameLabel: UILabel!
+    @IBOutlet weak var movieReleaseDateLabel: UILabel!
+    @IBOutlet weak var movieOverviewLabel: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
-        
         self.movieNameLabel.textColor = UIColor(hexString: APP_MOVIE_TITLE_FONT_COLOR)
-        
         self.movieReleaseDateLabel.textColor = UIColor(hexString: APP_MOVIE_RL_DATE_FONT_COLOR)
-        
         self.movieOverviewLabel.textColor = UIColor(hexString: APP_MOVIE_OVERVIEW_FONT_COLOR)
     }
-
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
         // Configure the view for the selected state
     }
-
 }
